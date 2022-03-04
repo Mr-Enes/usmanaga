1b6de48a4a4f60f62c7955026a86b1b2fd616c58
Mr.Enes <92262912+Mr-Enes@users.noreply.github.com>
2022-03-04 22:10:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ intents: 98303 });
+const config = require('./config.json');
+
+//Prefix Ayarlama
+var prefix = config.prefix
+
 
 //Ready.js
 client.on('ready', () => {
   console.log(`${client.user.tag} Çevirimiçi !`);
-  client.user.setActivity(`Safiyeyi`, {type: "WATCHING"});
-  client.user.setStatus("idle");
+  client.user.setStatus('idle');
+  client.user.setActivity('r!yardım');
 });
 
-//SA-AS KOMUTU
+//Sa-As Komutu
 client.on('messageCreate', message => {
-  if(message.content.toLowerCase() === "sa") {
-    message.channel.send(`${message.author} Aleykümselam Hoşgeldin !`);
+  if(message.content.toLowerCase() === 'sa') {
+    message.channel.send(`${message.author} Aleykümselam Hoşgeldin !`)
   };
 });
 
-//KOMİK KOMUTLAR #1
+//Yardım Menüsü
 client.on('messageCreate', message => {
-  if(message.content.toLowerCase() === "usmanım") {
-    message.channel.send(`${message.author} bRe SuSaK Sen Ne DeRSıN ?!`);
-  };
-});
-
-//Yardım Komutu
-client.on('messageCreate', async message => {
-  if(message.content.toLowerCase() === 'yardım') {
-
-    const yardım = new Discord.MessageEmbed()
+  
+    var yardım = new MessageEmbed()
     .setTitle("Yardım Menüsü")
-    .setColot(ff0000)
-    .setDescription("Yardım Menüsü Çalişür")
-    .addTimestamp()
+    .setColor("#ff0000")
+    .setDescription("Yardım Menüsü Çalışıyor")
+    .setTimestamp()
 
-    message.channel.send({ embeds: [yardım] });
-};
+if(message.content === prefix + 'yardım') {
+    message.channel.send({ embeds: [yardım] })
+  };
 });
 
 //Bot Giriş
-client.login('OTQ2MDIzMTQ2ODU3ODI4MzUy.YhYqtg.kIjXKatPspmp9uMXNKlOHODC5xk');
+client.login(config.token);
